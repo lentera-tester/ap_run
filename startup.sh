24003952e40830f773a98258ba5696665ca280c8
lentera-tester <lenteratester@gmail.com>
2024-07-03 00:24:20 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
+set -e
+
+# Apply database migrations
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start Gunicorn
+gunicorn --workers 2 --bind 0.0.0.0:8000 myproject.wsgi
